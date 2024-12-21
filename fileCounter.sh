6ee77ff98be4abb37afd7bff2f00db9c929038e5
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-12-21 20:46:44 +0545

    file count

--- fileCounter.sh
+++ fileCounter.sh
@@ -1,5 +1,18 @@
 # A Script that count files in the given folder
 
 #! /bin/bash
-echo "Give the path to count the file (/root/etc): "
-read path
+# echo "Give the path to count the file (/root/etc): "
+# read path
+read -p "Give the path to count the file (/root/etc): " path
+echo $path
+
+if [[ -d "$path" ]]; then
+    echo "Folder exists";
+    normal_count=$(find "$path" -maxdepth 1 -type f ! -name ".*" | wc -l)
+
+    hidden_count=$(find "$path" -maxdepth 1 -type f -name ".*" | wc -l)
+
+    echo "The folder contains $normal_count normal files and $hidden_count hidden files";
+else
+    echo "Folder doesn't exist";
+fi
